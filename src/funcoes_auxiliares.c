152ab8f718f80b6be848a07085467f90ef13f15d
Tomás Gonçalves <166638083+bauertommy4@users.noreply.github.com>
2024-05-17 03:48:08 +0100

    Issues Fixed: 9 digitos obrigatorios para o telefone, e formato Correto para o CC com uppercase(funcoes_auxiliares.c)

--- src/funcoes_auxiliares.c
+++ src/funcoes_auxiliares.c
@@ -16,6 +16,16 @@ int verifica_numeros(const char* input) {
     return 1; // É um Número
 }
 
+// verificar se tem 9 digitos (telefone)
+int verificar_telefone(const char *str) {
+    int comprimento = 0;
+    while (*str) {
+        comprimento++;
+        str++;
+    }
+    return comprimento >= 9;
+}
+
 // Função para verificar se é uma string
 int verifica_string(const char *str) {
     for (int i = 0; str[i] != '\0'; i++) {
@@ -39,13 +49,19 @@ int verifica_email(const char *str) {
 
 // Função para verificar se CC tem formato 12345678-9-AB1
 int verifica_cartao_cidadao(const char *str) {
-    int n1, n2;
+    int n1, n2, n3;
     char c1, c2;
-    if (sscanf(str, "%8d-%1d-%1c%1c", &n1, &n2, &c1, &c2) == 4) {
-        if (isupper(c1) && isupper(c2))
-            return 1; // CC válido
+    // verificar comprimento
+    if (strlen(str) != 13) {
+        return 0; // n é valido
     }
-    return 0; // CC inválido
+    if (sscanf(str, "%8d-%1d-%1c%1c%1d", &n1, &n2, &c1, &c2, &n3) == 5) { //verificar 8 nums / 1 num / 2 letras / 1 num
+        // letras maiuscluas?
+        if (isupper(c1) && isupper(c2)) { //verificar uppercase
+            return 1; // valido
+        }
+    }
+    return 0; // n é valido
 }
 
 // Função para Limpar o Buffer
@@ -53,8 +69,6 @@ void limpar_buffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
-
-
 
 // Função encontrar o nódulo do paciente pelo ID
 PACIENTES find_id(PACIENTES lista,int id){
@@ -66,21 +80,33 @@ PACIENTES find_id(PACIENTES lista,int id){
 
 
 
-// Função para o input de Números
-int input_numeros() {
+
+
+
+/////////////////////////////// Inputs ///////////////////////////////
+int input_numeros(int flag) {  //portanto, flag=1 -> numeros de telefone
     int num;
     char input[TAM_INT];
+
     while (1) {
         fgets(input, TAM_INT, stdin);
+
+        // Verifica se o input tem o caractere '\n' e o remove
         if (input[strlen(input) - 1] == '\n') {
-            input[strcspn(input, "\n")] = '\0';   // Removemos a primeira occorência de '\n'
+            input[strcspn(input, "\n")] = '\0';  // Removemos a primeira ocorrência de '\n'
+
             if (verifica_numeros(input)) {
-                sscanf(input, "%d", &num);
-                return num;
-            } else
+                if (flag == 1 && !verificar_telefone(input)) {
+                    printf("Número de telefone é composto por 9 dígitos! Insira novamente: ");
+                } else {
+                    sscanf(input, "%d", &num);
+                    return num;
+                }
+            } else {
                 printf("Número inválido. Por favor, insira dígitos válidos: ");
+            }
         } else {
-            printf("Número inválido. Por favor, insira um número dentro do tamanho do Buffer ( %d dígitos no Máximo ) : ", TAM_INT - 2);
+            printf("Número inválido. Por favor, insira um número dentro do tamanho do Buffer (%d dígitos no Máximo): ", TAM_INT - 2);
             limpar_buffer();
         }
     }
